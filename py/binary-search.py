5861d683a7c2bde946aec852342cf64d3b5bcd6c
Jesse-Tingle <jtingle0@gmail.com>
2021-02-11 22:18:48 -0500

    working out basics of binary search

---
+++ py/binary-search.py
@@ -0,0 +1,32 @@
+import random
+
+print("Get the computer to guess your number...")
+print("Tell me what two number's to guess beween.")
+min = int(input("min: "))
+max = int(input("max: "))
+# randomNumber = random.randint(min,max)
+magicNumber = int(input("What's the magic number?"))
+
+
+
+while (((min + max)/2).floor ):
+
+# while input_num != random_number:
+    
+
+#     input_num = int(input("Guess a number between 1-300: "))
+#     if input_num == random_number:
+#         print("Wooohoooo! You guessed it!")
+#     elif min < input_num:
+#         min = input_num + 1
+#         print("Your guess was too low.")
+#     elif input_num > max:
+#         max = input_num - 1 
+#         print("Your guess was too high.") 
+
+
+
+
+
+
+# print(input_num)
